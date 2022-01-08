3a1d0a024d5ad00c7669aee43e25e3d77d6fb3d5
gvolpe <volpegabriel@gmail.com>
2022-01-08 15:19:01 +0000

    Deploying to gh-pages from  @ b5b81411892d2eac390e39175b861f332de838a2 🚀

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Blog tools flake";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      forSystem = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          ruby = pkgs.ruby_2_7;
+          rubygems = (pkgs.rubygems.override { ruby = ruby; });
+          bundle = "${ruby}/bin/bundle";
+        in
+        {
+          devShell = pkgs.mkShell {
+            name = "blog-tools-shell";
+            buildInputs = [
+              ruby # 2.7.5p203
+              pkgs.python2 # 2.7.18
+            ];
+            shellHook = ''
+              mkdir -p .nix-gems
+              export GEM_HOME=$PWD/.nix-gems
+              export GEM_PATH=$GEM_HOME
+              export PATH=$GEM_HOME/bin:$PATH
+              ${bundle} install
+            '';
+          };
+        };
+    in
+    flake-utils.lib.eachDefaultSystem forSystem; # // app;
+}
